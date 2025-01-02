bb44f72ecc00f9d5d3929199adacb6645b9c8f4f
savya-27 <adudotlasavya123@gmail.com>
2025-01-02 20:16:25 +0530

    Atliq Hardware files

---
+++ Dataset/sql.sql
@@ -0,0 +1,7 @@
+SELECT * FROM gdb041.dim_customer;
+
+select distinct market from gdb041.dim_customer;
+
+select distinct platform from gdb041.dim_customer;
+
+select distinct channel from gdb041.dim_customer;
